b9f1cb5c2a0386db2b9e359de1fb9e49e5707393
waltenesjr <waltenesjr@live.com>
2017-09-13 17:32:07 -0300

    configuração do template principal e da tela de categoria

--- js/uiRouterConfig.js
+++ js/uiRouterConfig.js
@@ -1,6 +1,6 @@
 'use strict';
 
-detranApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+gerqApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
 
     //###################### INICIO DA DEFINICAO DO PADRAO DAS ROTAS ACESSAVEIS ######################
     $urlRouterProvider.otherwise("/categoria");
@@ -8,7 +8,7 @@ detranApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvide
     $stateProvider
         .state('categoria', {
             url: "/categoria",
-            templateUrl: "/pages/categoria.html",
+            templateUrl: "pages/categoria.html",
             controller: 'CategoriaController'
         })
     //###################### FIM DA DEFINICAO DO PADRAO DAS ROTAS ACESSAVEIS ######################
